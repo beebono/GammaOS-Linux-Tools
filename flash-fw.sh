c7a68dc0e71c1a45f97f520f528b235f46ef8396
Noxwell <beebono@yahoo.com>
2025-12-13 16:56:01 -0600

    Account for non-signed img files

--- flash-fw.sh
+++ flash-fw.sh
@@ -2,12 +2,26 @@
 adb reboot bootloader
 
 cd put-pac-in-here
+fastboot waitfordevice # Purposely non-functional command since getvar returns to stdout and stderr
 currentslot=$(fastboot getvar current-slot 2>&1 | awk 'NR==1{print $2}')
+if [[ -f "boot.img.signed" ]]; then
+    bootfile="boot.img.signed"
+else
+    bootfile="boot_${currentslot}.img"
+
+fi
+
+if [[ -f "vendor_boot.img.signed" ]]; then
+    vendbootfile="vendor_boot.img.signed"
+else
+    vendbootfile="vendor_boot_${currentslot}.img"
+fi
+
 superfile=$(ls -F super*.img)
 
-fastboot flash boot_$currentslot boot.img.signed
+fastboot flash boot_$currentslot $bootfile
 fastboot flash init_boot_$currentslot init_boot_$currentslot.img
-fastboot flash vendor_boot_$currentslot vendor_boot.img.signed
+fastboot flash vendor_boot_$currentslot $vendbootfile
 fastboot flash super $superfile
 fastboot erase userdata
 fastboot erase metadata
