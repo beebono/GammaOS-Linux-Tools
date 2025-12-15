ff0f52abbc9961a3bcd9e30bff6b3596e2ea07c3
Noxwell <beebono@yahoo.com>
2025-12-15 00:11:01 -0600

    Add stock vbmeta images and simplify flashing process

--- README.md
+++ README.md
@@ -17,13 +17,11 @@ sudo apt install build-essential libusb-1.0-0 libusb-1.0-0-dev
 ## Flashing Requirements
 
 - libusb
-- adb
-- fastboot
-- A fully downloaded and extracted GammaOSNext .pac file
+- A fully downloaded and decompressed GammaOSNext .pac file
 
 **Debian/Ubuntu**
 ```bash
-sudo apt install libusb-1.0-0 adb fastboot
+sudo apt install libusb-1.0-0
 ```
 
 ---
@@ -36,32 +34,9 @@ sudo apt install libusb-1.0-0 adb fastboot
 1. Copy your extracted `.pac` file into the `put-pac-in-here` folder.
 2. Run the `prepare-fw-files.sh` bash script from this repository.
 
-### Section 2: Unlock Bootloader 
+### Section 2: Unlock Bootloader and Flash GammaOSNext
 1. Power off your Unisoc T820-based handheld.
-2. Download and extract the `UnisocTools.7z` file from [GammaOSNext Releases](https://github.com/TheGammaSqueeze/GammaOSNext/releases/tag/v.1.0.0-T820).
-3. Copy the `spd_dump` executable and the `t820-unlock.sh` bash script from this repositrory into the `UnlockBootloader` folder from the extracted archive.
-    1. (Optional) Rename the `spd_dump.exe` file in that folder to something like `spd_dump_windows.exe` to avoid confusion.
-4. Open a terminal in the `UnlockBootloader` folder and run `t820-unlock.sh`.
+2. Open a terminal in the folder containing this repository and run `unlock-and-flash.sh`.
     1. While holding the **Back** button on your device, plug the device into your computer.
-    2. Your device will reboot after the unlock, and will likely power on to recovery with a scary warning. Don't panic and just perform the Factory Reset as instructed.
-
-### Section 3: Enable USB Debugging
-1. Once you are back in the Stock OS, enable **Developer Options**:
-    1. Open your **Settings** app.
-    2. Scroll all the way to the bottom and tap **About Device**.
-    3. Scroll down and find the **Build Number** area.
-    4. Tap it 7-10 times, until *"You are now a Developer!"* shows up at the bottom of your screen.
-    5. Go back one screen and tap **System**, then find and tap **Developer Options**
-        1. This may be hidden in a dropdown.
-2. Enable **USB Debugging**
-    1. Scroll down and tap the switch next to the **USB Debugging* option.
-    2. This will ask you to confirm, do so.
-    3. If your device is still plugged in, it will ask for authorization. Check the box and approve it.
-
-### Section 4: Flash GammaOSNext
-1. Plug your device into your computer and approve any popups that may show on the screen.
-1. Run the `flash-fw.sh` bash script from this repository.
-    1. This will take a few minutes. Grab a drink or stand up for a bit!
-    2. Your device should reboot into GammaOSNext when done.
-
-2. Enjoy!
+    2. Follow the instructions printed in your terminal from this point.
+    3. Your device should reboot into GammaOSNext when done.
--- unlock-and-flash.sh
+++ unlock-and-flash.sh
@@ -7,12 +7,20 @@ if [[ -f "super.img" ]]; then
 elif [[ -f "super_lite.img" ]]; then
     superfile="super_lite.img"
 else
-    echo "No super*.img found. Did you unpack your firmware?" >&2
+    echo "No super image found. Did you unpack your firmware?" >&2
     exit 1
 fi
 
+if [[ -f "uboot_b.img(1)" ]]; then
+    ubootafile="uboot_b.img(1)"
+    ubootbfile="uboot_b.img(1)" 
+else
+    ubootafile="uboot_a.img"
+    ubootbfile="uboot_b.img"
+fi
+
 if [[ -f "boot.img.signed" ]]; then
-    bootafile="boot.img.signed(1)"
+    bootafile="boot.img.signed"
     bootbfile="boot.img.signed(1)"
 else
     bootafile="boot_a.img"
@@ -20,7 +28,7 @@ else
 fi
 
 if [[ -f "vendor_boot.img.signed" ]]; then
-    vendbootafile="vendor_boot.img.signed(1)"
+    vendbootafile="vendor_boot.img.signed"
     vendbootbfile="vendor_boot.img.signed(1)"
 else
     vendbootafile="vendor_boot_a.img"
@@ -28,7 +36,7 @@ else
 fi
 
 if [[ -f "dtbo.img.signed" ]]; then
-    dtboafile="dtbo.img.signed(1)"
+    dtboafile="dtbo.img.signed"
     dtbobfile="dtbo.img.signed(1)"
 else
     dtboafile="dtbo_a.img"
@@ -46,25 +54,34 @@ sudo ./spd_dump --wait 600 \
     fdl ./unisoc-blobs/sml_a.bin 0xbcfffe00 \
     exec
 
-read -n 1 -r -s -p $'Bootloader unlocked! Wait for your device to get to the charging screen.\nThen, please unplug your device and get ready to replug it while holding the BACK button again.\nPress any key when you are ready...\n'
+read -n 1 -r -s -p $'\nBootloader unlocked! Wait for your device to get to the charging screen.\nThen, please unplug your device and get ready to replug it while holding the BACK button again.\nPress any key when you are ready...\n'
 
 sudo ./spd_dump --wait 600 \
-    exec_addr 0x65012f48 \
     fdl ./put-pac-in-here/fdl1-dl.bin 0x65000800 \
     fdl ./put-pac-in-here/fdl2-dl.bin 0xb4fffe00 \
     exec \
-    w super ./put-pac-in-here/$superfile \
+    w uboot_a ./put-pac-in-here/$ubootafile \
+    w uboot_b ./put-pac-in-here/$ubootbfile \
     w boot_a ./put-pac-in-here/$bootafile \
     w boot_b ./put-pac-in-here/$bootbfile \
     w vendor_boot_a ./put-pac-in-here/$vendbootafile \
     w vendor_boot_b ./put-pac-in-here/$vendbootbfile \
     w dtbo_a ./put-pac-in-here/$dtboafile \
     w dtbo_b ./put-pac-in-here/$dtbobfile \
-    write_parts_a ./put-pac-in-here \
-    write_parts_b ./put-pac-in-here \
-    set_active a \
+    w super ./put-pac-in-here/$superfile \
+    w vbmeta_a ./stock-blobs/vbmeta.bin \
+    w vbmeta_b ./stock-blobs/vbmeta.bin \
+    w vbmeta_system_a ./stock-blobs/vbmeta_system.bin \
+    w vbmeta_system_b ./stock-blobs/vbmeta_system.bin \
+    w vbmeta_vendor_a ./stock-blobs/vbmeta_vendor.bin \
+    w vbmeta_venodr_b ./stock-blobs/vbmeta_vendor.bin \
+    w vbmeta_system_ext_a ./stock-blobs/vbmeta_system_ext.bin \
+    w vbmeta_system_ext_b ./stock-blobs/vbmeta_system_ext.bin \
+    w vbmeta_product_a ./stock-blobs/vbmeta_product.bin \
+    w vbmeta_product_b ./stock-blobs/vbmeta_product.bin \
+    w vbmeta_odm_a ./stock-blobs/vbmeta_odm.bin \
+    w vbmeta_odm_b ./stock-blobs/vbmeta_odm.bin \
+    e misc \
+    e metadata \
     reset
-
-
-
 
